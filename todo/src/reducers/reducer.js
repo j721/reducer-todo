c059789e4492f6c25b7b132afbb48058f4216265
j721 <juliadvu@gmail.com>
2020-04-13 16:07:19 -0500

    Changes made to reducer function

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,22 +1,27 @@
 //Initialize state for todo items
 export const intialState =[{
-    todo:'',
+    item:'Learn about reducers',
     completed: false,
     id: 3892987589
-}]
-
+}];
 //reducer function- updates and manipulates state
-export const reducer (state, action) =>{
+export function reducer (state, action) {
     //switch statement. simplified if/else statement
     switch(action.type){
         case 'ADD_TODO':
-            return{
-                ...state, action.payload
-            }
+            return[
+                ...state,
+                { item: action.payload, completed:false, id: Date.now() } 
+            ]
         case "COMPLETED_TODO":
-            return state.map((item)=>{
-                return item.id ===action.payload
+            return state.map((todo)=>{
+                return todo.id ===action.payload?
+                {...todo, completed: !todo.completed}:todo
             })
+        case "CLEAR_COMPLETED_TODO":
+            return state.filter((item)=> !item.completed)
+        default: 
+            return state; 
     }
 
 }
