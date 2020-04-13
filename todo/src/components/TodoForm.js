c5e647aabfc7cb46eeb9f629db1ef4e02f88db9b
j721 <juliadvu@gmail.com>
2020-04-13 18:02:05 -0500

    Working on TodoForm component

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-const TodoForm = ({dispatch})=>{
+// const TodoForm = ({dispatch})=>{
+//     const [item, setItem] = useState("");
+
+//     const handleChanges = e =>{
+//         setItem(e.target.value);
+//     }
+
+//     const submitItem=e =>{
+//         e.preventDefault();
+//         dispatch({type: "ADD_TODO",payload: item});
+//         setItem("");
+//     }
+// }
+
+function TodoForm(props){
     const [item, setItem] = useState("");
-
-    const handleChanges = e =>{
-        setItem(e.target.value);
-    }
-
-    const submitItem=e =>{
-        e.preventDefault();
-        dispatch({type: "ADD_TODO",payload: item});
-        setItem("");
-    }
+    return(
+        <form onSubmit ={(event)=>{
+            event.preventDefault();
+            props.addTodo(form);
+            setForm('');
+        }}>
+            <input
+                name ='todo'
+                placeholder ='Add'
+                value ={form}
+                onChange ={(event)=>{
+                    setItem(event.target.value);
+                }}
+            /><button type ='submit'>Add Todo</button>
+        </form>
+    )
 }
